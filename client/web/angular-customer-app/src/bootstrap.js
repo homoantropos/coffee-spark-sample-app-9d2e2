42685753693455a98b39df54e28f9eca8cc115f6
Firebase Studio <noreply@firebase.studio>
2025-09-09 08:45:53 +0000

    Initialized workspace with Firebase Studio

---
+++ client/web/angular-customer-app/src/bootstrap.js
@@ -0,0 +1,12 @@
+window["APP_TEMPLATE_BOOTSTRAP"] = {
+    geminiApiKey: "AIzaSyAs8AqXh97TaA8ohoEJMXNm6Brs-rV7sGk",
+
+    firebase: {
+      apiKey: "AIzaSyBjzYnyBRYnsh3rlD_aMa2zXw14DhMGSXM",
+      authDomain: "coffee-spark-sample-app-9d2e2.firebaseapp.com",
+      projectId: "coffee-spark-sample-app-9d2e2",
+      storageBucket: "coffee-spark-sample-app-9d2e2.firebasestorage.app",
+      messagingSenderId: "413772980970",
+      appId: "1:413772980970:web:11cf06a2830a6ab0e12d93",
+    }
+    };
